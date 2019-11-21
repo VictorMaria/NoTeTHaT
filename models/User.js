234851e7debe2db71398683dc5c18c62b489dcbe
Victor Abayomi <victor.abayomi@outlook.com>
2019-11-21 16:49:23 +0100

    chore(setup): Set up backend environment Initialize express server using babel Set up eslint with airbnb style guide Set up mongoDB and mongoose Add db models [Finishes #169279552]

---
+++ models/User.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+  },
+});
+
+const User = mongoose.model('user', UserSchema);
+
+export default User;
