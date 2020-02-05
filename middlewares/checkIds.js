1901b2563c04634ead5b79b8fc54c7059e2f273d
Victor Abayomi <victor.abayomi@outlook.com>
2020-02-05 13:01:34 +0100

    feature(notes): select and delete notes create endpoint to let users select and delete multiple notes at once [Finishes #169279545]

--- middlewares/checkIds.js
+++ middlewares/checkIds.js
@@ -1,11 +1,11 @@
 const checkIds = (req, res, next) => {
   const noteIds = req.body.id;
   const checkArray = Array.isArray(noteIds);
-  if (checkArray === false && typeof noteIds === 'string') {
+  if (checkArray === false && typeof noteIds !== 'string') {
     return res.status(400).json({ message: 'Atleast one id is required' });
   }
   if (typeof noteIds === 'string' && !noteIds.match(/^[0-9a-f]{24}$/)) {
-    return res.status(400).json({ message: 'ID is not valid' });
+    return res.status(400).json({ message: 'id is not valid' });
   }
   if (Array.isArray(noteIds) && (noteIds.filter((noteId) => !noteId.match(/^[0-9a-f]{24}$/)).length > 0)) {
     return res.status(400).json({ message: 'Ensure all ids are valid' });
